a88cc151527c499354e6f0cdb8b34cb9354256dd
VladimirMuravlev <122268023+VladimirMuravlev@users.noreply.github.com>
2025-12-14 00:14:03 +0300

    Update sw.js
    обновил скрипт

--- sw.js
+++ sw.js
@@ -10,14 +10,13 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .catch((err) => console.warn('Кэширование не удалось:', err))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
